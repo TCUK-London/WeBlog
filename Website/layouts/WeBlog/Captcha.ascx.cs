37c94fe9bd4a0751bda79be16f972fbcf21aad5e
Anna Jerofejeva <Anna Jerofejeva@anna-laptop.headoffice.trueclarity.net>
2015-10-29 10:45:46 +0000

    Enabled captcha max and min timeout settings

--- Website/layouts/WeBlog/Captcha.ascx.cs
+++ Website/layouts/WeBlog/Captcha.ascx.cs
@@ -6,13 +6,20 @@ namespace Sitecore.Modules.WeBlog.Layouts
 {
 	public partial class Captcha : System.Web.UI.UserControl
 	{
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            if (uxCaptchaCode != null)
+            {
+                uxCaptchaCode.CaptchaMaxTimeout = (int)Settings.CaptchaMaximumTimeout.TotalSeconds;
+                uxCaptchaCode.CaptchaMinTimeout = (int)Settings.CaptchaMinimumTimeout.TotalSeconds;
+            }
+        }
+
         protected void uxCaptchaValidator_ServerValidate(object source, ServerValidateEventArgs args)
         {
             if (uxCaptchaCode != null)
             {
                 uxCaptchaCode.ValidateCaptcha(uxCaptchaText.Text);
-                uxCaptchaCode.CaptchaMaxTimeout = (int)Settings.CaptchaMaximumTimeout.TotalSeconds;
-                uxCaptchaCode.CaptchaMinTimeout = (int)Settings.CaptchaMinimumTimeout.TotalSeconds;
                 args.IsValid = uxCaptchaCode.UserValidated;
             }
         }
